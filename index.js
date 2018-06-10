e9b96398ad4c280906da5e38a87cdca13d7e1c4f
ArvinLleva <arvin.lleva@yahoo.com>
2018-06-10 22:32:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,15 @@
 var dodger = document.getElementById('dodger')
 
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(leftNumbers, 10)
+  var rightNumbers = dodger.style.right.replace('px', '');
+  var right = parseInt(leftNumbers, 10);
  
   if (left > 0) {
-    dodger.style.left = `${left - 1}px`
+    dodger.style.left = `${left - 1}px`;
   }
 }
 
-document.addEventListener('keydown', function(e)){
-  if(e.which ===)
-}
+document.addEventListener('keydown', function(e){
+  if(e.which ===39) // 39 is the ascii key-value for right
+  moveDodgerRight();
+})
