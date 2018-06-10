a0cc201d83c636addee06099a7b88340766c393e
ArvinLleva <arvin.lleva@yahoo.com>
2018-06-10 23:10:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,32 +4,11 @@ var dodger = document.getElementById('dodger')
 
 // we have to recall that the black box is using the coordinates of (dodger.style.left,dodger.style.bottom)
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.left.replace('px', ''); // replace(searchvalue, newvalue)
+  var rightNumbers = dodger.style.left.replace('px', ''); // replace(searchvalue, newvalue) 
+  // we don't use d
   var right = parseInt(rightNumbers, 10); //parseInt(string, radix)
  
-  if (right > 0) {
-    dodger.style.right = `${right + 1}px`;
+  if (right > 0) { // this prevents the box from going all the way to the right
+    dodger.style.right = `${right + 1}px`; 
   }
 }
-
-document.addEventListener('keydown', function(e){
-  if(e.which === 39){ // 39 is the ascii key-value for right
-  moveDodgerRight();
-  }
-})
-
-
-var dodger = document.getElementById('dodger')
-function moveDodgerLeft() {
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
- 
-  if (left > 0) {
-    dodger.style.left = `${left + 1}px`
-  }
-}
-document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    moveDodgerLeft()
-  }
-})
