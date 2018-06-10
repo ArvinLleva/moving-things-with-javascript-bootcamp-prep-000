7f3d1571b323a450fd16cb02c57770de318fca64
ArvinLleva <arvin.lleva@yahoo.com>
2018-06-10 23:14:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var dodger = document.getElementById('dodger')
 // we have to recall that the black box is using the coordinates of (dodger.style.left,dodger.style.bottom)
 function moveDodgerRight() {
   var rightNumbers = dodger.style.left.replace('px', ''); // replace(searchvalue, newvalue) 
-  // we don't use d
+  // we don't use dodger.style.right 
   var right = parseInt(rightNumbers, 10); //parseInt(string, radix)
  
   if (right > 0) { // this prevents the box from going all the way to the right
