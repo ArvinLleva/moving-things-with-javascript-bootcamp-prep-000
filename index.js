4f672206499a10b0db5768119017469a15e3928c
ArvinLleva <arvin.lleva@yahoo.com>
2018-06-10 22:29:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much."
 function moveDodgerRight() {
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(leftNumbers, 10)
  
   if (left > 0) {
     dodger.style.left = `${left - 1}px`
