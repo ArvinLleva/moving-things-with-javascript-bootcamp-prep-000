ef9cacfc56058f93b8051d71d77b014bbc16efc3
ArvinLleva <arvin.lleva@yahoo.com>
2018-06-10 22:32:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var dodger = document.getElementById('dodger')
 
 function moveDodgerRight() {
   var rightNumbers = dodger.style.right.replace('px', '');
-  var right = parseInt(leftNumbers, 10);
+  var right = parseInt(rightNumbers, 10);
  
   if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
+    dodger.style.right = `${left - 1}px`;
   }
 }
 
